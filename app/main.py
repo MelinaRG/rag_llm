bbab991b6ce9a1f55ca3c11cd5b221477a7acc4c
MelinaRG <meligriffo@gmail.com>
2024-02-22 12:46:23 -0300

    add comments

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from .use_cases.chatbot import GenerateAnswer
 
 app = FastAPI()
 
+# Definición de la ruta POST para el endpoint
 @app.post(
         "/askme",
         operation_id= 'askme_chatbot'
@@ -11,6 +12,7 @@ async def generate_answer(
     user_name : str,
     question : str
 ):
+    # Llamada a la función para generar la respuesta
     result = GenerateAnswer(user_name, question).execute()
 
     return result
--- app/use_cases/chatbot.py
+++ app/use_cases/chatbot.py
@@ -4,39 +4,44 @@ import cohere
 from langdetect import detect
 import os
 
+# Clase para generar la respuesta del chatbot
 class GenerateAnswer:
     def __init__(self, user_name: str, question: str):
+        # Inicialización de los atributos de la instancia
         self.user_name = user_name
         self.question = question
+        # Inicialización de modelos y servicios externos
         self.__model = SentenceTransformer('all-MiniLM-L6-v2') #384 dimensional
         self.__index = self.__init_pinecone()
         self.__co = cohere.Client(os.environ.get('COHERE_API_KEY'))
 
+    # Llamada a la función para generar la respuesta
+    def execute(self) -> str:
+        result = self.ask(self.question, self.user_name)
+        return result
+    
+    # Inicialización del pinecone
     def __init_pinecone(self):
         pc = Pinecone(api_key=os.environ.get('PINECONE_API_KEY'),)
         return pc.Index(os.environ.get('PINECONE_INDEX'))
 
+    #Embbeding de la pregunta y busqueda de similitudes
     def __find_match(self, question: str):
         input_em = self.__model.encode(question).tolist()
         result = self.__index.query(vector=input_em, top_k=1, includeMetadata=True)
         return result["matches"][0]["metadata"]["text"]
 
+    # Generación de la respuesta a traves del LLM
     def __generate_answer(self, prompt: str):
-
         response = self.__co.chat(
             message=prompt,
             model="command",
             temperature=0.1,
             max_tokens=50
         )
-
         return response.text
         
-
-    def execute(self) -> str:
-        result = self.ask(self.question, self.user_name)
-        return result
-
+    #Llamada para iniciar el proceso de generación de respuesta
     def ask(self, question: str, user_name: str) -> str:
         context = self.__find_match(question)
         language = self.detect_language(question)
@@ -44,7 +49,7 @@ class GenerateAnswer:
         result = self.__generate_answer(prompt)
         return result
 
-
+    # Generación del prompt
     def generate_header(self, question: str, user_name: str, context: str, language: str) -> str:
         return f"""Tu tarea es responder la siguiente pregunta: '{question}' del usuario '{user_name}',
                     utiliza emojis en tus respuestas y responde siempre en base a la información proporcionada
@@ -55,6 +60,7 @@ class GenerateAnswer:
                     - user: ¿Cómo se llama el perro? answer: El perro se llama Beto.
                     - user: ¿Where is she from? answer: She is from Canadá.
                    """
+    #Detección del lenguaje para mejorar el prompt
     def detect_language(self, text: str) -> str:
         try:
             return detect(text)
